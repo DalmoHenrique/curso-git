2f60181596c51a842ce0e0140ed4177b1addc763
Dalmo Henrique <dalmohenriquee@hotmail.com>
2019-05-06 18:50:39 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ 024GitBash/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _024GitBash {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Teste Git Bash!");
+            Console.ReadLine();
+        }
+    }
+}
