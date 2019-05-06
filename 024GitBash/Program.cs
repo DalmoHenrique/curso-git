82f89c7ecee212d6df573572831dbd0ec8cf9089
Dalmo Henrique <dalmohenriquee@hotmail.com>
2019-05-06 18:52:23 -0300

    Acrescentada uma mensagem de bom dia

--- 024GitBash/Program.cs
+++ 024GitBash/Program.cs
@@ -4,6 +4,7 @@ namespace _024GitBash {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Teste Git Bash!");
+            Console.WriteLine("Bom dia!");
             Console.ReadLine();
         }
     }
